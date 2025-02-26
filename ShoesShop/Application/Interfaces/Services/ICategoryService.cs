e72f51aec1ad6a16e783d47caa73d4e8551d1bbf
Long <Long@ADMIN-PC>
2025-02-26 22:13:29 +0700

    cate service

---
+++ ShoesShop/Application/Interfaces/Services/ICategoryService.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using ShoesShop.Application.DTOs;
+using ShoesShop.Domain.Entities;
+
+namespace ShoesShop.Application.Interfaces.Services
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<Category>> GetAllAsync(int pageSize,int pageNum);
+
+        Task<bool> CreateCategoryAsync(CreateCateDTO model);
+        Task<Category> GetCategoryByNameAsync(string name);
+        Task<bool> UpdateStatusAsync(int CategoryID);
+    }
+}
