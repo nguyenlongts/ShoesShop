17aa18348420031cf3c66eb2498706c827a24180
Long <Long@ADMIN-PC>
2025-02-26 21:57:45 +0700

    add DTO

---
+++ ShoesShop/Application/DTOs/CreateProductDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace ShoesShop.Application.DTOs
+{
+    public class CreateProductDTO
+    {
+        public  string ProductName { get; set; }
+
+        public string Description { get; set; }
+
+        public int BrandID { get; set; }
+    }
+}
