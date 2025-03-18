51cdcb89d7dc84f38e69039706bf6ec61615e8e3
Long <Long@ADMIN-PC>
2025-03-18 22:08:10 +0700

    add new

---
+++ ShoesShop/Application/DTOs/UserInfoResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace ShoesShop.Application.DTOs
+{
+    public class UserInfoResponse
+    {
+        public string FullName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+    }
+}
