d8db2e730df3acdf80455c753d714bed8fea31df
Long <Long@ADMIN-PC>
2025-03-19 21:55:30 +0700

    DTO for order

---
+++ ShoesShop/Application/DTOs/CreateOrderItemDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace ShoesShop.Application.DTOs
+{
+    public class CreateOrderItemDto
+    {
+        public int ProductDetailId { get; set; } // ID của sản phẩm
+        public int Quantity { get; set; }        // Số lượng sản phẩm
+        public decimal PriceAtOrder { get; set; } // Giá sản phẩm tại thời điểm đặt hàng
+    }
+
+}
