95875645126cbf78a7ef10126b82a80cf79274b0
Long <Long@ADMIN-PC>
2025-03-19 21:55:42 +0700

    add new

---
+++ ShoesShop/Application/Interfaces/Repositories/IOrderRepository.cs
@@ -0,0 +1,34 @@
+﻿using ShoesShop.Domain.Entities;
+
+namespace ShoesShop.Application.Interfaces.Repositories
+{
+    public interface IOrderRepository
+    {
+
+        Task<IEnumerable<Order>> GetAllOrdersAsync(int pageNum,int pageSize);
+
+
+        Task<IEnumerable<Order>> GetOrdersByUserIdAsync(string userId);
+
+
+        Task<Order> GetOrderByIdAsync(Guid orderId);
+
+
+        Task<Order> CreateOrderAsync(Order order);
+
+
+        Task<bool> UpdateOrderStatusAsync(Guid orderId, Order.OrderStatus status);
+
+
+        Task<bool> DeleteOrderAsync(Guid orderId);
+
+        Task<IEnumerable<OrderItem>> GetOrderItemsAsync(Guid orderId);
+
+
+        Task<bool> AddOrderItemAsync(OrderItem orderItem);
+
+
+        Task<bool> OrderExistsAsync(Guid orderId);
+    }
+
+}
