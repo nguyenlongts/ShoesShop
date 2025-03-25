993f0dff532491865ab5f89ba9aefd0de5707fe8
Long <Long@ADMIN-PC>
2025-03-25 17:18:06 +0700

    implement MailService

---
+++ ShoesShop/Application/Services/MailService.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+using System.Net.Mail;
+using ShoesShop.Application.Interfaces.Services;
+
+namespace ShoesShop.Application.Services
+{
+    public class MailService : IEmailService
+    {
+
+        private readonly IConfiguration _config;
+        public MailService(IConfiguration config)
+        {
+            _config = config;
+        }
+        public async Task SendMailAsync(string address, string subject, string body)
+        {
+            var smtpClient = new SmtpClient(_config["Smtp:Host"])
+            {
+                Port = int.Parse(_config["Smtp:Port"]),
+                Credentials = new NetworkCredential(_config["Smtp:Username"], _config["Smtp:Password"]),
+                EnableSsl = true,
+            };
+
+            var mail = new MailMessage
+            {
+                From = new MailAddress("noreply@shoesshop.com", "ShoesShop Support"),
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = true,
+            };
+            mail.To.Add(address);
+
+            await smtpClient.SendMailAsync(mail);
+        }
+    }
+}
