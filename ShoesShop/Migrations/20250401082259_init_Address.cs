8ac09e83b65500fd7470aa2666f5f6a282b4fff2
Long <Long@ADMIN-PC>
2025-04-01 21:20:31 +0700

    update

---
+++ ShoesShop/Migrations/20250401082259_init_Address.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShoesShop.Migrations
+{
+    /// <inheritdoc />
+    public partial class init_Address : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "District",
+                table: "Addresses");
+
+            migrationBuilder.DropColumn(
+                name: "Province",
+                table: "Addresses");
+
+            migrationBuilder.DropColumn(
+                name: "Ward",
+                table: "Addresses");
+
+            migrationBuilder.RenameColumn(
+                name: "Street",
+                table: "Addresses",
+                newName: "FullAddress");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FullAddress",
+                table: "Addresses",
+                newName: "Street");
+
+            migrationBuilder.AddColumn<string>(
+                name: "District",
+                table: "Addresses",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Province",
+                table: "Addresses",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Ward",
+                table: "Addresses",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
