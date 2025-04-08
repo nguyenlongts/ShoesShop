73d81b86248bd67984d3a462d6ca21b8e200bfe7
Long <Long@ADMIN-PC>
2025-04-08 23:36:22 +0700

    move logic from controller to service

---
+++ ShoesShop/Application/Interfaces/Repositories/IProductDetailRepository.cs
@@ -0,0 +1,16 @@
+﻿
+using ShoesShop.Application.DTOs;
+using ShoesShop.Domain.Entities;
+
+namespace ShoesShop.Application.Interfaces.Repositories
+{
+    public interface IProductDetailRepository
+    {
+        Task<List<ProductDetailDTO>> GetProductDetailDtosByProductIdAsync(int productId);
+        Task<List<ProductDetail>> GetByIdsAsync(List<int> ids);
+        Task<ProductDetail?> GetByIdAsync(int id);
+        Task AddAsync(ProductDetail productDetail);
+        Task<bool> ExistsAsync(int productId, int colorId, int sizeId);
+        Task SaveChangesAsync();
+    }
+}
